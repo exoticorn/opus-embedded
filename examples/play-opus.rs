699e878c48074340152f9ea8b7c220bbde614ed3
Dennis Ranke <dennis.ranke@gmail.com>
2024-01-05 17:02:28 +0100

    play back opus file with standard crates

--- examples/play-opus.rs
+++ examples/play-opus.rs
@@ -1,3 +1,66 @@
+use std::{env, fs::File, time::Duration};
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    BufferSize, SampleRate, StreamConfig,
+};
+
 fn main() {
-    println!("Hello");
+    let mut ogg_reader = ogg::PacketReader::new(File::open(env::args().nth(1).unwrap()).unwrap());
+    let mut opus_decoder = opus::Decoder::new(48000, opus::Channels::Mono).unwrap();
+    let mut sample_buffer = [0i16; 8192];
+    let mut read_position = 0;
+    let mut write_position = 0;
+
+    _ = ogg_reader.read_packet().unwrap(); // id
+    _ = ogg_reader.read_packet().unwrap(); // comment
+
+    let host = cpal::default_host();
+    let device = host.default_output_device().unwrap();
+    let stream = device
+        .build_output_stream(
+            &StreamConfig {
+                channels: 1,
+                sample_rate: SampleRate(48000),
+                buffer_size: BufferSize::Default,
+            },
+            move |mut buffer: &mut [i16], _: &cpal::OutputCallbackInfo| {
+                while !buffer.is_empty() {
+                    if write_position == 0 {
+                        if let Some(packet) = ogg_reader.read_packet().unwrap() {
+                            write_position = opus_decoder
+                                .decode(&packet.data, &mut sample_buffer, false)
+                                .unwrap();
+                        } else {
+                            for sample in buffer {
+                                *sample = 0;
+                            }
+                            break;
+                        }
+                    } else {
+                        let copy_size = buffer.len().min(write_position - read_position);
+                        buffer[..copy_size].copy_from_slice(
+                            &sample_buffer[read_position..read_position + copy_size],
+                        );
+                        read_position += copy_size;
+                        if read_position == write_position {
+                            read_position = 0;
+                            write_position = 0;
+                        }
+                        buffer = &mut buffer[copy_size..];
+                    }
+                }
+            },
+            move |err| {
+                eprintln!("Output stream error: {}", err);
+            },
+            None,
+        )
+        .unwrap();
+
+    stream.play().unwrap();
+
+    loop {
+        std::thread::sleep(Duration::from_secs(1));
+    }
 }
