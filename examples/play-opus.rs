18bebeddb0862c6eb8f58f147d46551cfa1e8f5b
Dennis Ranke <dennis.ranke@gmail.com>
2024-01-14 21:11:45 +0100

    clean up example

--- examples/play-opus.rs
+++ examples/play-opus.rs
@@ -1,70 +1,68 @@
-use std::{env, fs::File, io::Read, time::Duration};
+use std::{env, fs::File, time::Duration};
 
+use anyhow::{anyhow, Result};
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
     BufferSize, SampleRate, StreamConfig,
 };
 
-fn main() {
-    let mut opus_file = File::open(env::args().nth(1).unwrap()).unwrap();
-    let mut ogg_reader = ogg_embedded::Reader::<16384, 1024>::new();
+const CHANNEL_COUNT: usize = 2;
 
-    let decoder_object_buffer = vec![0u8; opus_embedded::decoder_size(1)].leak();
-    let mut opus_decoder = opus_embedded::Decoder::new(decoder_object_buffer, 48000, 1);
+fn main() -> Result<()> {
+    // use ogg crate to demuc ogg/opus file to simplify testing
+    // note that this crate might not be viable for embedded
+    let mut ogg_reader = ogg::PacketReader::new(File::open(
+        env::args()
+            .nth(1)
+            .ok_or_else(|| anyhow!("Opus file parameter missing"))?,
+    )?);
 
-    let mut sample_buffer = [0i16; 8192];
+    // skip id and comment headers
+    _ = ogg_reader.read_packet_expected()?;
+    _ = ogg_reader.read_packet_expected()?;
+
+    // create &'static mut buffer for the opus decoder object
+    // on embedded, you'd probably just use an array in a static_cell for this
+    let decoder_object_buffer = vec![0u8; opus_embedded::decoder_size(CHANNEL_COUNT)].leak();
+    let mut opus_decoder = opus_embedded::Decoder::new(decoder_object_buffer, 48000, CHANNEL_COUNT);
+
+    // buffer for decoded opus frame, contains data in range read_position..write_position
+    let mut sample_buffer = [0i16; 8192 * CHANNEL_COUNT];
     let mut read_position = 0;
     let mut write_position = 0;
-    let mut packet_index = 0;
-
-    let mut file_buffer = [0u8; 1024];
-    let mut file_buffer_size = 0;
-    let mut file_buffer_pos = 0;
 
     let host = cpal::default_host();
     let device = host.default_output_device().unwrap();
     let stream = device
         .build_output_stream(
             &StreamConfig {
-                channels: 1,
+                channels: CHANNEL_COUNT as u16,
                 sample_rate: SampleRate(48000),
                 buffer_size: BufferSize::Default,
             },
-            move |mut buffer: &mut [i16], _: &cpal::OutputCallbackInfo| {
-                while !buffer.is_empty() {
-                    if write_position == 0 {
-                        if let Some(packet) = ogg_reader.next_packet() {
-                            if packet_index >= 2 {
-                                write_position =
-                                    opus_decoder.decode(packet, &mut sample_buffer, false);
-                            }
-                            packet_index += 1;
+            move |mut output_buffer: &mut [i16], _: &cpal::OutputCallbackInfo| {
+                while !output_buffer.is_empty() {
+                    if read_position == write_position {
+                        // no more data in buffer, read and decode next packet
+                        if let Some(packet) = ogg_reader.read_packet().unwrap() {
+                            write_position =
+                                opus_decoder.decode(&packet.data, &mut sample_buffer, false)
+                                    * CHANNEL_COUNT;
+                            read_position = 0;
                         } else {
-                            if file_buffer_pos < file_buffer_size {
-                                file_buffer_pos += ogg_reader
-                                    .write(&file_buffer[file_buffer_pos..file_buffer_size]);
-                            } else {
-                                file_buffer_size = opus_file.read(&mut file_buffer).unwrap();
-                                file_buffer_pos = 0;
-                                if file_buffer_size == 0 {
-                                    for sample in buffer {
-                                        *sample = 0;
-                                    }
-                                    break;
-                                }
+                            for sample in output_buffer {
+                                *sample = 0;
                             }
+                            break;
                         }
                     } else {
-                        let copy_size = buffer.len().min(write_position - read_position);
-                        buffer[..copy_size].copy_from_slice(
+                        // copy samples from sample_buffer into output_buffer
+                        let copy_size = output_buffer.len().min(write_position - read_position);
+                        output_buffer[..copy_size].copy_from_slice(
                             &sample_buffer[read_position..read_position + copy_size],
                         );
                         read_position += copy_size;
-                        if read_position == write_position {
-                            read_position = 0;
-                            write_position = 0;
-                        }
-                        buffer = &mut buffer[copy_size..];
+                        output_buffer = &mut output_buffer[copy_size..];
                     }
                 }
             },
